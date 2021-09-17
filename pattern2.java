52bf22ead0a444d8c73daf0f4c4c91d8a33fa528
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2021-09-17 21:44:28 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+public class pattern2 {
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the value of x");
+        // System.out.println("Enter the value of y");
+        // int x=sc.nextInt();
+        // int y=sc.nextInt();
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println( " ");
+        }
+    }
+}
